4eebc3d901a1b4d75e099e1001e05eb69ccce8a9
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-09-06 15:32:39 +0530

    feat: add StringProblems folder with SumOfNum program

---
+++ StringProblems/SumOfNum.java
@@ -0,0 +1,23 @@
+package StringProblems;
+import java.util.Scanner;
+public class SumOfNum
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many numbers do you want to enter?");
+        int n = scanner.nextInt();
+        int [] num = new int[n];
+        int sum= 0;
+        for(int i =0 ; i<num.length ; i++)
+        {
+            System.out.print("Enter number "+ (i+1)+" : ");
+            num[i]= scanner.nextInt();
+            sum += num[i];
+        }
+        System.out.println("Total value : "+sum);
+        scanner.close();
+
+    }
+    
+
+}

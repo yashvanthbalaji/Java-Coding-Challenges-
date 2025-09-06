8a6316fb69eaa03b8603c32bff3296059b9f207f
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-09-06 15:27:03 +0530

    feat:add Pyramid patter program

---
+++ PatternPrograms/Pyramid.java
@@ -0,0 +1,24 @@
+package PatternPrograms;
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[]args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = scanner.nextInt();
+        for(int i = 1 ; i<=n ; i++)
+        {
+            for (int j =i;j<=n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}

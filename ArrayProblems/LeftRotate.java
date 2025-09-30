91a60b1eb97a8d7d334dc4cd9bee4ebefdc434c4
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-09-30 10:24:48 +0530

    feat: implement left rotation of array by K positions with input validation
    - Added logic to perform left rotation on user-defined array
    
    - Included modular method for rotation with optimization (k % n)
    
    - Added input validation to check for non-positive array size
    
    - Cleanly displays original and processed arrays

---
+++ ArrayProblems/LeftRotate.java
@@ -0,0 +1,54 @@
+package ArrayProblems;
+import java.util.Scanner;
+public class LeftRotate
+{
+    public static int[] leftRotateByK(int[]arr,int k)
+    {
+        int first;
+        k=k%arr.length;
+        for(int i =1 ; i<=k;i++)
+        {
+            first = arr[0];
+            for(int j =0 ; j < arr.length-1;j++)
+            {   
+                arr[j]=arr[j+1];
+            }
+            arr[arr.length-1] = first;
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the array size :"); 
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements");
+        for(int i = 0 ; i< arr.length ; i++)
+        {
+            System.out.print("Element "+(i+1)+": " );
+            arr[i]=scanner.nextInt();
+        }
+        System.out.print("Original array : ");
+        for (int num : arr)
+        {
+            System.out.print(num+" ");
+            
+        }
+        System.out.println();
+        System.out.print("Enter K positions : ");
+        int k = scanner.nextInt();
+        arr = leftRotateByK(arr,k);
+        System.out.print("Processed array : ");
+        for(int num : arr)
+        {
+            System.out.print(num+" "); 
+        }
+
+
+        scanner.close();
+    }
+    
+}
